ec345ceb730c914eb7d825399e8677ebed760627
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-17 23:49:54 -0500

    added consts to Layout

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -2,9 +2,12 @@ import { Dimensions } from 'react-native';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+const margin = 15
 
 export default {
-  margin: 15,
+  margin,
+  borderWidth: 2,
+  borderRadius: margin,
   window: {
     width,
     height,
