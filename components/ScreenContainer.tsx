c4d20b7e5977e78b325d9d4645cada66588c53d5
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-17 23:49:20 -0500

    defined container component for all screens

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -1,12 +1,16 @@
-import { View } from "react-native";
+import React from "react";
+import Colors from "../constants/Colors";
+import Layout from "../constants/Layout";
+import useColorScheme from "../hooks/useColorScheme";
+import { View, ViewProps } from "./Themed";
 
-
-export default function (props: View['props']) {
+export default function (props: ViewProps) {
+  const colorScheme = useColorScheme()
+  const { style } = props;
   return (
-    // <View>
-    // {/* <HorizontalLine /> */}
-    <View style={{ flex: 1 }} {...props} />
-    // {/* <HorizontalLine /> */}
-    // </View>
+    <View
+      {...props}
+      style={[{ flex: 1, backgroundColor: Colors[colorScheme].background }, style]}
+    />
   )
 }
