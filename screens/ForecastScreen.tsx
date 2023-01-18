3d41ed80503d09a267801994dae7fbbcc991724d
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-17 23:51:40 -0500

    Added searchBar and keyboardAvoidingView

--- screens/ForecastScreen.tsx
+++ screens/ForecastScreen.tsx
@@ -1,16 +1,46 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import React from 'react';
+import { useState } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { useHeaderHeight } from '@react-navigation/elements'
 import ScreenContainer from '../components/ScreenContainer';
+import SearchBar from '../components/SearchBar';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import Layout from '../constants/Layout';
 
 export default function ({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [searchQuery, setSearchQuery] = useState<string>('')
+
   return (
-    <ScreenContainer>
-      <Text>
-        asdfjkl;
-      </Text>
-    </ScreenContainer>
+
+    <ScreenContainer >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={useHeaderHeight()}
+        style={{ flex: 1 }}
+      >
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ textAlign: 'center' }}>
+            Daily forecasts (past, present, and future) will be displayed here.
+          </Text>
+        </View>
+
+
+
+        <SearchBar
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          placeholder="Current Location"
+        />
+
+      </KeyboardAvoidingView >
+
+    </ScreenContainer >
+
+
+
+
   );
 }
