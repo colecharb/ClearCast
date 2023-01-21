fbbb64d6b534d538e00b916682f5279ba6cba217
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-20 20:09:35 -0500

    weather API object types

--- types.tsx
+++ types.tsx
@@ -33,3 +33,106 @@ export type RootTabScreenProps<Screen extends keyof RootTabParamList> = Composit
   BottomTabScreenProps<RootTabParamList, Screen>,
   NativeStackScreenProps<RootStackParamList>
 >;
+
+export type Conditions = {
+  "id": number,
+  "main": string,
+  "description": string,
+  "icon": string
+}
+
+export type Weather = {
+  "coord": {
+    "lon": number,
+    "lat": number
+  },
+  "weather": Conditions[],
+  "base": string,
+  "main": {
+    "temp": number,
+    "feels_like": number,
+    "temp_min": number,
+    "temp_max": number,
+    "pressure": number,
+    "humidity": number,
+    "sea_level": number,
+    "grnd_level": number
+  },
+  "visibility": number,
+  "wind": {
+    "speed": number,
+    "deg": number,
+    "gust": number
+  },
+  "rain": {
+    "1h": number
+  },
+  "clouds": {
+    "all": number
+  },
+  "dt": number,
+  "sys": {
+    "type": number,
+    "id": number,
+    "country": string,
+    "sunrise": number,
+    "sunset": number
+  },
+  "timezone": number,
+  "id": number,
+  "name": string,
+  "cod": number
+}
+
+export type ForecastInterval_3hr = {
+  "dt": number,
+  "main": {
+    "temp": number,
+    "feels_like": number,
+    "temp_min": number,
+    "temp_max": number,
+    "pressure": number,
+    "sea_level": number,
+    "grnd_level": number,
+    "humidity": number,
+    "temp_kf": number
+  },
+  "weather": Conditions[],
+  "clouds": {
+    "all": number
+  },
+  "wind": {
+    "speed": number,
+    "deg": number,
+    "gust": number
+  },
+  "visibility": number,
+  "pop": number,
+  "rain": {
+    "3h": number
+  },
+  "sys": {
+    "pod": string
+  },
+  "dt_txt": string
+}
+
+export type Forecast = {
+  "cod": string,
+  "message": number,
+  "cnt": number,
+  "list": ForecastInterval_3hr[],
+  "city": {
+    "id": number,
+    "name": string,
+    "coord": {
+      "lat": number,
+      "lon": number
+    },
+    "country": string,
+    "population": number,
+    "timezone": number,
+    "sunrise": number,
+    "sunset": number
+  }
+}
