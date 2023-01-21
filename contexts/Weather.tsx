d72ce6f6fd73a19880719a1aae40f9b22bcb7fb7
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-20 20:13:50 -0500

    use new weather types, URL management

--- contexts/Weather.tsx
+++ contexts/Weather.tsx
@@ -1,59 +1,10 @@
 import React, { createContext, useState, useEffect, useRef } from 'react';
 import * as Location from 'expo-location';
-
-export type WeatherData = {
-  "coord": {
-    "lon": number,
-    "lat": number
-  },
-  "weather": [
-    {
-      "id": number,
-      "main": string,
-      "description": string,
-      "icon": string
-    }
-  ],
-  "base": string,
-  "main": {
-    "temp": number,
-    "feels_like": number,
-    "temp_min": number,
-    "temp_max": number,
-    "pressure": number,
-    "humidity": number,
-    "sea_level": number,
-    "grnd_level": number
-  },
-  "visibility": number,
-  "wind": {
-    "speed": number,
-    "deg": number,
-    "gust": number
-  },
-  "rain": {
-    "1h": number
-  },
-  "clouds": {
-    "all": number
-  },
-  "dt": number,
-  "sys": {
-    "type": number,
-    "id": number,
-    "country": string,
-    "sunrise": number,
-    "sunset": number
-  },
-  "timezone": number,
-  "id": number,
-  "name": string,
-  "cod": number
-}
+import { Forecast, Weather } from '../types';
 
 export type WeatherContextData = {
   location: React.MutableRefObject<Location.LocationObject | undefined>,
-  currentWeather: WeatherData | undefined,
+  currentWeather: Weather | undefined,
   getCurrentWeatherAsync: () => Promise<void>,
   errorMessage: string | undefined,
 };
@@ -73,7 +24,12 @@ export const WeatherProvider = ({ children }: { children: any }) => {
   const [currentWeather, setCurrentWeather] = useState<WeatherData>()
 
   const API_KEY = 'f82f1ad0af0d696e1c657915946d75c2'
-  // const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${57}&lon=${-2.15}&appid=${API_KEY}&units=imperial`
+  const CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather`
+  const FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast`
+  // easy way to generate params to append either API URL
+  const makeUrlParams = (lat: number, lon: number, units: 'imperial' | 'metric') => (
+    `?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${units}`
+  )
 
   async function getCurrentWeatherAsync() {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -87,10 +43,11 @@ export const WeatherProvider = ({ children }: { children: any }) => {
       location.current = loc;
       // console.log(loc);
       const { latitude, longitude } = loc.coords;
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=imperial`)
+      fetch(CURRENT_WEATHER_URL + makeUrlParams(latitude, longitude, 'imperial'))
         .then((response) => response.json())
         .then((response) => {
           setCurrentWeather(response)
+          // console.log('here');
           console.log('here');
         })
         .catch((error) => console.error(error))
