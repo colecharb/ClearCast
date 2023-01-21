632d8ebd816b70237361026b3479f5c4380cd330
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-20 22:38:56 -0500

    margin lil bigger

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -2,7 +2,7 @@ import { Dimensions } from 'react-native';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-const margin = 15
+const margin = 16
 
 export default {
   margin,
