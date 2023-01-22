daa8a93f4a06c1e3e9d296837ee69f559339f997
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-22 03:32:47 -0500

    ForecastIntervalCard has become HourForecastCard

---
+++ components/HourForecastCard.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef } from "react";
+import { Image, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+import { WeatherContext } from "../contexts/Weather";
+import useColorScheme from "../hooks/useColorScheme";
+import { HourlyForecast } from "../types";
+import Card from "./Card";
+import { Text, View } from "./Themed";
+
+
+export default function ({ forecast, index }: { forecast: HourlyForecast | undefined, index: number }) {
+
+
+  if (!forecast) return null
+
+
+  const forecastInterval = forecast.list[index]
+
+  const date = new Date(forecastInterval.dt * 1000)
+  const hour = date.toLocaleTimeString(navigator.language, { hour: 'numeric' })
+
+  if (!forecast) return null
+
+  return (
+    <Card>
+
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+
+        <Text>{hour}</Text>
+
+        <View style={{ aspectRatio: 1, height: 45, marginVertical: -16 }}>
+          <Image
+            style={styles.weatherIcon}
+            source={{ uri: `https://openweathermap.org/img/wn/${forecastInterval.weather[0].icon}@4x.png` }}
+          />
+        </View>
+
+        <Text>L {forecastInterval.main.temp_min.toFixed(0)}˚</Text>
+        <Text>H {forecastInterval.main.temp_max.toFixed(0)}˚</Text>
+
+
+        <TempInterval
+          minTemp={forecast?.minTemp}
+          temp={forecastInterval?.main.temp}
+          maxTemp={forecast?.maxTemp}
+        />
+
+        {/* <Text>{forecastInterval.main.temp.toFixed(0)}˚</Text> */}
+
+      </View>
+    </Card>
+  )
+}
+
+const TempInterval = ({ minTemp, temp, maxTemp }: { minTemp: number, temp: number, maxTemp: number }) => (
+  <View style={{ width: 150, height: '100%', flexDirection: 'row', backgroundColor: '#444' }}>
+    <View style={{ flex: temp - minTemp }} />
+    <View style={{ width: 5, height: '100%', backgroundColor: 'red' }} />
+    <View style={{ flex: maxTemp - temp }} />
+  </View>
+)
+
+const styles = StyleSheet.create({
+  weatherIcon: {
+    aspectRatio: 1,
+    width: undefined,
+    height: undefined
+  }
+})
