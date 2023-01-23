f9abeeaa2aeeaa483a23efc52c68f80805640c9c
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-23 01:40:00 -0500

    this has gradient at the top now

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -1,21 +1,28 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import Colors from "../constants/Colors";
 import Layout from "../constants/Layout";
 import useColorScheme from "../hooks/useColorScheme";
 import GradientOverlay from "./GradientOverlay";
 import { View, ViewProps } from "./Themed";
+import { useHeaderHeight } from '@react-navigation/elements'
 
 export default function (props: ViewProps) {
-  const colorScheme = useColorScheme()
+  const theme = useColorScheme()
+  const headerHeight = useHeaderHeight()
   const { style } = props;
   return (
     <>
-      <GradientOverlay />
-    <View
-      {...props}
-      style={[{ flex: 1, backgroundColor: Colors[colorScheme].background }, style]}
-    />
-    </>
 
+      <View
+        {...props}
+        style={[{ flex: 1, backgroundColor: Colors[theme].background }, style]}
+      />
+      <LinearGradient
+        style={{ position: 'absolute', top: 0, left: 0, right: 0, height: headerHeight }}
+        colors={[Colors[theme].background, Colors[theme].background + '77', Colors[theme].background + '00']}
+        locations={[0, 3 / 4, 1]}
+      />
+    </>
   )
 }
