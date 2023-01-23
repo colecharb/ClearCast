85d0a5c0cddbc5389ecbdb16a4fff904b1723c25
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-22 21:58:22 -0500

    gradient overlay is built into screenContainer

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import Colors from "../constants/Colors";
 import Layout from "../constants/Layout";
 import useColorScheme from "../hooks/useColorScheme";
+import GradientOverlay from "./GradientOverlay";
 import { View, ViewProps } from "./Themed";
 
 export default function (props: ViewProps) {
   const colorScheme = useColorScheme()
   const { style } = props;
   return (
+    <>
+      <GradientOverlay />
     <View
       {...props}
       style={[{ flex: 1, backgroundColor: Colors[colorScheme].background }, style]}
     />
+    </>
+
   )
 }
