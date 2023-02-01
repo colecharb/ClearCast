dcd6d829ca7fc6db8c76c85401cfd7920d59b26b
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-01 00:40:47 -0500

    removed unused import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ForecastProvider, WeatherProvider } from './contexts/Weather';
+import { WeatherProvider } from './contexts/Weather';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
