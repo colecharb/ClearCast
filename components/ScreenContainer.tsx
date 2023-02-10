e723dacee8c13ae8254033e53faad9c7f7a6135e
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-10 13:56:30 -0500

    useSafeAreaInsets

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -4,10 +4,12 @@ import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
 import { View, ViewProps } from "./Themed";
 import { useHeaderHeight } from '@react-navigation/elements'
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function (props: ViewProps) {
-  const theme = useColorScheme()
-  const headerHeight = useHeaderHeight()
+  const theme = useColorScheme();
+  const headerHeight = useHeaderHeight();
+  const safeAreaInsets = useSafeAreaInsets();
   const { style } = props;
   return (
     <>
@@ -17,7 +19,7 @@ export default function (props: ViewProps) {
       />
       <LinearGradient
         pointerEvents="none"
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, height: headerHeight }}
+        style={{ position: 'absolute', top: 0, left: 0, right: 0, height: safeAreaInsets.top * 1.25 }}
         colors={[Colors[theme].background, Colors[theme].background + 'aa', Colors[theme].background + '00']}
         locations={[0, 3 / 4, 1]}
       />
