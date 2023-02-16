7b4aa0004fbda74523111184e1eacd169a4ca367
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-15 21:24:48 -0500

    taller header to make gradient smoother

--- components/ClearCastHeader.tsx
+++ components/ClearCastHeader.tsx
@@ -14,6 +14,8 @@ export default function () {
   const weather = useContext(WeatherContext);
   const safeAreaInsets = useSafeAreaInsets();
 
+
+  // todo: do this intelligently: ignore null entries and make it less wordy
   const placeName = (
     weather.place ? (
       `${weather.place.city}, ${weather.place.region}`
@@ -26,18 +28,18 @@ export default function () {
     <LinearGradient
       colors={[Colors[theme].background + 'ff', Colors[theme].background + 'cc', Colors[theme].background + '00']}
       locations={[0, 1 / 2, 1]} //{[0, 1 / 4, 2 / 4, 3 / 4, 1]}
-      style={{ padding: Layout.margin, justifyContent: 'center', paddingTop: safeAreaInsets.top }}
+      style={{ paddingBottom: Layout.margin * 4, justifyContent: 'center', paddingTop: safeAreaInsets.top, marginBottom: -Layout.margin * 3 }}
       pointerEvents='box-none'
     >
       <Text style={{ fontWeight: '200', textAlign: 'center', fontSize: 24, color: Colors[theme].text, marginBottom: Layout.margin / 2 }}>
         {'ClearCast'}
       </Text>
-      {!weather.loading ? (
+      {weather.loading ? (
+        <ActivityIndicator />
+      ) : (
         <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: '900' }}>
           {placeName}
-        </Text>
-      ) : (
-        <ActivityIndicator />
+          </Text>
       )}
 
     </LinearGradient>
