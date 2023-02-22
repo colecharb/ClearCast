3211fe6352a542970bd7cb293897fb096566b8a6
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-22 00:06:14 -0500

    made this pressable

--- components/SettingBase.tsx
+++ components/SettingBase.tsx
@@ -1,23 +1,22 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import React, { Children } from "react";
-import { LayoutAnimation, StyleSheet } from "react-native";
+import { LayoutAnimation, Pressable, StyleSheet } from "react-native";
 import Colors from "../constants/Colors";
 import Layout from "../constants/Layout";
 import useColorScheme from "../hooks/useColorScheme";
 import { Text, View } from "./Themed";
 
-export default function ({ text, children }: { text: string, children: any }) {
+export default function ({ text, onPress, children }: { text: string, onPress?: () => void, children: any }) {
 
   const styles = makeStyles();
 
   return (
-    <View style={styles.settingContainer}>
+    <Pressable style={styles.settingContainer} onPress={onPress}>
       <Text style={styles.settingText}>{text}</Text>
       {children}
-    </View>
+    </Pressable>
   )
 }
-
 
 const makeStyles = () => {
 
@@ -31,6 +30,7 @@ const makeStyles = () => {
       padding: Layout.margin
     },
     settingText: {
+      flex: 1, 
       fontSize: 18,
       fontWeight: 'normal',
       color: Colors[theme].text,
