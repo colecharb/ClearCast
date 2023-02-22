e31c7afdc7be668ea7bc549eecb475cd53051b2c
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-22 00:08:34 -0500

    settings overhaul;
    Chages to state in weather context will be applied on GoBack()

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,34 +1,90 @@
-import React, { useContext, useState } from "react";
-import { Button, ScrollView } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, FlatList, LayoutAnimation, ScrollView, StyleSheet } from "react-native";
 import { useHeaderHeight } from '@react-navigation/elements'
 import ScreenContainer from "../components/ScreenContainer";
 import SettingBase from "../components/SettingBase";
-import { Text } from "../components/Themed";
+import { Text, View } from "../components/Themed";
 import Colors from "../constants/Colors";
-import makeStyles from "../constants/Styles";
 import { WeatherContext } from "../contexts/Weather";
 import useColorScheme from "../hooks/useColorScheme";
-import { RootStackScreenProps } from "../types";
-
-type TemperatureUnits = 'metric' | 'imperial';
+import { ALL_UNITS, RootStackScreenProps, Units } from "../types";
+import DropDownPicker from 'react-native-dropdown-picker';
+import Layout from "../constants/Layout";
+import { FontAwesome } from "@expo/vector-icons";
+import HorizontalLine from "../components/HorizontalLine";
+import { HeaderBackButton } from '@react-navigation/elements'
+import { color } from "@rneui/themed/dist/config";
 
 export default function ({ navigation }: RootStackScreenProps<'Settings'>) {
 
   const weather = useContext(WeatherContext);
   const theme = useColorScheme();
   const headerHeight = useHeaderHeight();
+  const styles = makeStyles();
+
+  const [units, setUnits] = useState(weather.units);
+
+  navigation.setOptions({
+    headerLeft: () => (
+      <HeaderBackButton
+        labelVisible
+        tintColor={Colors[theme].tint}
+        onPress={() => {
+          weather.setUnits(units);
+          navigation.goBack();
+        }}
+      />
+    )
+  })
+
+  const UnitsSelection = ({ item }: { item: Units }) => {
+    return (
+      <SettingBase
+        text={item}
+        onPress={() => setUnits(item)}
+      >
+        {units === item ? (
+          <FontAwesome name='check' color={Colors[theme].tint} />
+        ) : (
+          null
+        )}
+      </SettingBase>
+    )
+  }
 
   return (
     <ScreenContainer>
-      <ScrollView contentContainerStyle={{ paddingTop: headerHeight }}>
-        <SettingBase text="Temperature Units">
-          <Button
-            title={weather.temperatureUnits}
-            onPress={() => weather.toggleTemperatureUnits()}
-            color={Colors[theme].tint}
-          />
-        </SettingBase>
+      <ScrollView contentContainerStyle={{ padding: Layout.margin, paddingTop: headerHeight }}>
+        <Text style={styles.settingTitle}>
+          units
+        </Text>
+        <View style={styles.settingsView}>
+          {ALL_UNITS.map((units, index) => <>
+            {index === 0 ? null : <HorizontalLine />}
+            <UnitsSelection item={units} key={units} />
+          </>)}
+        </View>
       </ScrollView>
     </ScreenContainer>
   )
+}
+
+
+const makeStyles = () => {
+
+  const theme = useColorScheme();
+
+  return StyleSheet.create({
+    settingsView: {
+      backgroundColor: Colors[theme].subtle,
+      // margin: Layout.margin,
+      borderRadius: Layout.margin,
+    },
+    settingTitle: {
+      textTransform: 'uppercase',
+      color: Colors[theme].medium,
+      padding: Layout.margin,
+      marginTop: Layout.margin,
+    }
+  })
 }
