e74dc452f76cbdc346a10f3b7a770223bb7d9dfe
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-22 00:06:23 -0500

    layout tweaks

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -2,11 +2,11 @@ import { Dimensions } from 'react-native';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-const margin = 8
+const margin = 10
 
 export default {
   margin,
-  borderWidth: 2,
+  borderWidth: 1,
   borderRadius: 30,
   window: {
     width,
