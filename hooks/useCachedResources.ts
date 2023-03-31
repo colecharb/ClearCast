545aa96b3bd21348d392d8c4f54243319c5a8354
colecharb <40375497+colecharb@users.noreply.github.com>
2023-03-31 17:36:18 -0400

    load settings in this custom hook.
    set loading to complete only after everything has loaded.

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -2,32 +2,66 @@ import { FontAwesome } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
+import { STORAGE_KEYS, SettingsData, defaultSettings } from '../contexts/Settings';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const [storedSettings, setStoredSettings] = useState({} as SettingsData)
 
   // Load any resources or data that we need prior to rendering the app
   useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
-        SplashScreen.preventAutoHideAsync();
-
         // Load fonts
         await Font.loadAsync({
           ...FontAwesome.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
         });
-      } catch (e) {
+      } catch (error) {
         // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hideAsync();
+        console.warn("ERROR loading fonts with loadResourcesAndDataAsync()."), error;
       }
     }
 
-    loadResourcesAndDataAsync();
+    async function loadStoredSettingsAsync(): Promise<void> {
+      try {
+
+        const settingsSerialized = await AsyncStorage.getItem(
+          STORAGE_KEYS.SETTINGS
+        ).catch((error) => { });
+
+        const settings: SettingsData = (() => {
+          if (!settingsSerialized) {
+            console.log('No settings found in local storage; Using default settings.');
+            return defaultSettings;
+          }
+
+          console.log('Loaded serialized settings from local storage.');
+          return JSON.parse(settingsSerialized) as SettingsData;
+
+        })();
+
+        console.log('settings =', JSON.stringify(settings));
+        setStoredSettings(settings)
+
+      } catch (error) {
+        console.log("ERROR inside loadStoredSettingsAsync().", error)
+      } finally {
+        //loading finished
+      }
+    }
+
+    // call above loading functions only before first render
+    SplashScreen.preventAutoHideAsync();
+    Promise.all([
+      loadResourcesAndDataAsync(),
+      loadStoredSettingsAsync(),
+    ]).finally(() => {
+      setLoadingComplete(true);
+      SplashScreen.hideAsync();
+    });
   }, []);
 
-  return isLoadingComplete;
+  return { isLoadingComplete, storedSettings };
 }
